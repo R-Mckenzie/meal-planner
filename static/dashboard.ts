3f998d7e040f02bcf830ea6a107eb5c09f0e9606
Ross Mckenzie <rossmckenzie11@gmail.com>
2023-02-11 00:24:17 +0000

    organise and comment dashboard.ts

--- static/dashboard.ts
+++ static/dashboard.ts
@@ -1,7 +1,12 @@
-let recipes = document.querySelectorAll(".recipe-item") as NodeListOf<HTMLElement>;
-let dropzones = document.querySelectorAll('.dropzone') as NodeListOf<HTMLElement>;
-let dragged: HTMLElement | null;
 
+// Get the nosurf csrf token
+const csrfToken = document.querySelector<HTMLElement>(".dashboard-container")?.dataset.csrf
+
+/* DRAG AND DROP
+ * 	- Adds drag and drop event listeners for nodes and dropzones
+ * 	- Copying nodes from recipe list, moving nodes already on calendar
+ */
+let dragged: HTMLElement | null; // Dragged is the currently held drag and drop node
 const addNodeListeners = (node: HTMLElement) => {
 	node.addEventListener('dragstart', function() {
 		dragged = node;
@@ -22,12 +27,14 @@ const addNodeListeners = (node: HTMLElement) => {
 	});
 }
 
+let recipes = document.querySelectorAll(".recipe-item") as NodeListOf<HTMLElement>; // Includes the recipe list and meals on calendar
 recipes.forEach((r) => {
 	addNodeListeners(r);
 });
 
+// If the node is in the recipe list, delete from database.
+// If node is in the calendar, delete it from the page but not the database
 var addDeleteListener = (b: HTMLElement) => {
-	const container = document.querySelector<HTMLElement>(".dashboard-container")
 	if (b.parentNode) {
 		const node: HTMLElement = b.parentNode as HTMLElement
 		b.addEventListener('click', () => {
@@ -36,19 +43,19 @@ var addDeleteListener = (b: HTMLElement) => {
 			} else {
 				let rID: number = node.dataset.recipeid ? +node.dataset.recipeid : -1;
 				fetch("/recipes", {
-					method: "DELETE", body: JSON.stringify({ recipeID: +rID, csrf: container?.dataset.csrf })
+					method: "DELETE", body: JSON.stringify({ recipeID: +rID, csrf: csrfToken })
 				})
 				node.remove()
 			}
 		})
 	}
 }
-
 var deleteButtons: NodeListOf<HTMLElement> = document.querySelectorAll<HTMLElement>(".delete-button");
 deleteButtons.forEach((b) => {
 	addDeleteListener(b)
 });
 
+let dropzones = document.querySelectorAll('.dropzone') as NodeListOf<HTMLElement>;
 dropzones.forEach((z) => {
 	z.addEventListener('dragover', function(e) {
 		e.preventDefault();
@@ -56,13 +63,13 @@ dropzones.forEach((z) => {
 	z.addEventListener('dragleave', function(e) {
 		e.preventDefault();
 	});
+	// If we are dragging from another dropzone, move the node.
+	// If we are dragging from the recipe list, copy it.
 	z.addEventListener('drop', function(e) {
 		if (!dragged) return;
 		if (dragged?.classList.contains("meal-item")) {
-			// If we are dragging from another date, move the node.
 			z.append(dragged)
 		} else {
-			// If we are dragging from the recipe list, copy it.
 			let nodeCopy: HTMLElement = dragged.cloneNode(true) as HTMLElement;
 			nodeCopy.id = Date.now().toString();
 			nodeCopy.classList.add("meal-item")
@@ -74,9 +81,13 @@ dropzones.forEach((z) => {
 	});
 });
 
-
-// DATES =========================
-function setToMonday(date: Date) {
+/* DATES 
+ * 	- handles getting the date fromt the date elemtent,
+ * 	  which comes from the backend, and getting the first day of that week (Monday)
+ * 	- Moving forward and back a week with the prev and next buttons. 
+ * 	  This sends a request to the backend to reload the page with the new week's meals
+ */
+const setToMonday = (date: Date) => {
 	var day = date.getDay() || 7;
 	if (day !== 1)
 		date.setHours(-24 * (day - 1));
@@ -87,14 +98,13 @@ let dateElement = document.getElementById("date")
 let monday = setToMonday(new Date(Date.parse(dateElement?.textContent || "")));
 
 const changeDate = () => {
-	// const queryDate = monday.getDate() + "-" + (monday.getMonth() + 1). + "-" + monday.getFullYear()
 	const queryDate = monday.toISOString().slice(0, 10)
+	// Save meals?
 	window.location.replace("/dashboard?date=" + queryDate)
 }
 
 const prevBtn = document.getElementById("prev-week") as HTMLElement
 const nextBtn = document.getElementById("next-week") as HTMLElement
-
 prevBtn.addEventListener("click", function() {
 	monday.setDate(monday.getDate() - 7)
 	changeDate()
@@ -106,7 +116,10 @@ nextBtn.addEventListener("click", function() {
 })
 
 
-// SAVING MEAL PLAN =================
+/* SAVING MEALS 
+ * 	- Scans the dropzones and gets node recipeIDs and their respective dates
+ * 	- Sends a post request with the meal data and CSRF token for the backend to save
+ */
 type Meal = {
 	recipeID: number,
 	date: Date,
@@ -131,8 +144,7 @@ const getMeals = () => {
 
 const saveButton = document.querySelector(".save-button") as HTMLElement
 saveButton.addEventListener("click", () => {
-	const container = document.querySelector<HTMLElement>(".dashboard-container")
 	fetch("/dashboard", {
-		method: "POST", body: JSON.stringify({ weekStart: monday, meals: getMeals(), csrf: container?.dataset.csrf })
+		method: "POST", body: JSON.stringify({ weekStart: monday, meals: getMeals(), csrf: csrfToken })
 	}).then(() => location.reload())
 })
