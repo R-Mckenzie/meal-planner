e5a15e2faa160aa1d1e7e29b515bd7ee18a9ded0
Ross Mckenzie <rossmckenzie11@gmail.com>
2023-02-13 14:16:42 +0000

    comment JS code

--- static/recipeList.ts
+++ static/recipeList.ts
@@ -1,18 +1,20 @@
+// Listener to delete recipe from database when clicked
 var deleteButtons: NodeListOf<HTMLElement> = document.querySelectorAll<HTMLElement>(".delete-button");
 deleteButtons.forEach((b) => {
-	const recipesContainer = document.querySelector<HTMLElement>("#recipesList")
+	const csrfToken = document.querySelector<HTMLElement>("#recipesList")?.dataset.csrf
 	if (b.parentNode) {
 		const node: HTMLElement = b.parentNode as HTMLElement
 		b.addEventListener('click', () => {
 			let rID: number = node.dataset.recipeid ? +node.dataset.recipeid : -1;
 			fetch("/recipes", {
-				method: "DELETE", body: JSON.stringify({ recipeID: +rID, csrf: recipesContainer?.dataset.csrf })
+				method: "DELETE", body: JSON.stringify({ recipeID: +rID, csrf: csrfToken })
 			})
 			node.remove()
 		})
 	}
 });
 
+// Listener to redirect to the recipes edit page
 var recipeNode = document.querySelectorAll<HTMLElement>(".recipe-item");
 recipeNode.forEach((n) => {
 	const recipeID = n.dataset.recipeid;
